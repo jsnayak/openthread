912c4d2fe4950f41d055ca0aa14625d4bfdf56ee
J Nayak <j.snayakk@gmail.com>
2019-09-06 14:38:55 +0530

    Testing the push of changes

--- examples/platforms/posix/radio.c
+++ examples/platforms/posix/radio.c
@@ -25,7 +25,7 @@
  *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-
+// TESTING FOR GIT
 #include "platform-posix.h"
 
 #include <openthread/dataset.h>
